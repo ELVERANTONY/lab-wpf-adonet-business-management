bb6a3fc90a855982a53c9c0595cda40d5fdbf286
ELVERANTONY <elver.cholan@tecsup.edu.pe>
2026-09-23 16:41:25 -0500

    Constructor sin parametros en MainViewModel para instanciar repositorios por defecto

--- NeptunoApp/ViewModels/MainViewModel.cs
+++ NeptunoApp/ViewModels/MainViewModel.cs
@@ -1,20 +1,70 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using NeptunoApp.Data;
 
 namespace NeptunoApp.ViewModels;
-public partial class MainViewModel(ProductosViewModel productos,CategoriasViewModel categorias,ProveedoresViewModel proveedores,PedidosViewModel pedidos,ReportesViewModel reportes) : ObservableObject
+
+public partial class MainViewModel : ObservableObject
 {
- public ProductosViewModel Productos{get;}=productos;public CategoriasViewModel Categorias{get;}=categorias;public ProveedoresViewModel Proveedores{get;}=proveedores;public PedidosViewModel Pedidos{get;}=pedidos;public ReportesViewModel Reportes{get;}=reportes;
- [ObservableProperty] private string paginaActual="Productos";
- public bool MostrarProductos=>PaginaActual=="Productos";
- public bool MostrarCategorias=>PaginaActual=="Categorias";
- public bool MostrarProveedores=>PaginaActual=="Proveedores";
- public bool MostrarPedidos=>PaginaActual=="Pedidos";
- public bool MostrarReportes=>PaginaActual=="Reportes";
- partial void OnPaginaActualChanged(string value){OnPropertyChanged(nameof(MostrarProductos));OnPropertyChanged(nameof(MostrarCategorias));OnPropertyChanged(nameof(MostrarProveedores));OnPropertyChanged(nameof(MostrarPedidos));OnPropertyChanged(nameof(MostrarReportes));}
- [RelayCommand] private void VerProductos()=>PaginaActual="Productos";
- [RelayCommand] private void VerCategorias()=>PaginaActual="Categorias";
- [RelayCommand] private void VerProveedores()=>PaginaActual="Proveedores";
- [RelayCommand] private void VerPedidos()=>PaginaActual="Pedidos";
- [RelayCommand] private void VerReportes()=>PaginaActual="Reportes";
+    public ProductosViewModel Productos { get; }
+    public CategoriasViewModel Categorias { get; }
+    public ProveedoresViewModel Proveedores { get; }
+    public PedidosViewModel Pedidos { get; }
+    public ReportesViewModel Reportes { get; }
+
+    public MainViewModel()
+    {
+        var prodRepo = new ProductoRepository();
+        var catRepo = new CategoriaRepository();
+        var provRepo = new ProveedorRepository();
+        var pedRepo = new PedidoRepository();
+
+        Productos = new ProductosViewModel(prodRepo);
+        Categorias = new CategoriasViewModel(catRepo);
+        Proveedores = new ProveedoresViewModel(provRepo);
+        Pedidos = new PedidosViewModel(pedRepo);
+        Reportes = new ReportesViewModel(pedRepo);
+
+        // Cargar datos iniciales
+        _ = Productos.CargarCommand.ExecuteAsync(null);
+        _ = Categorias.CargarCommand.ExecuteAsync(null);
+        _ = Proveedores.CargarCommand.ExecuteAsync(null);
+        _ = Pedidos.CargarCommand.ExecuteAsync(null);
+    }
+
+    public MainViewModel(
+        ProductosViewModel productos,
+        CategoriasViewModel categorias,
+        ProveedoresViewModel proveedores,
+        PedidosViewModel pedidos,
+        ReportesViewModel reportes)
+    {
+        Productos = productos;
+        Categorias = categorias;
+        Proveedores = proveedores;
+        Pedidos = pedidos;
+        Reportes = reportes;
+    }
+
+    [ObservableProperty] private string paginaActual = "Productos";
+    public bool MostrarProductos => PaginaActual == "Productos";
+    public bool MostrarCategorias => PaginaActual == "Categorias";
+    public bool MostrarProveedores => PaginaActual == "Proveedores";
+    public bool MostrarPedidos => PaginaActual == "Pedidos";
+    public bool MostrarReportes => PaginaActual == "Reportes";
+
+    partial void OnPaginaActualChanged(string value)
+    {
+        OnPropertyChanged(nameof(MostrarProductos));
+        OnPropertyChanged(nameof(MostrarCategorias));
+        OnPropertyChanged(nameof(MostrarProveedores));
+        OnPropertyChanged(nameof(MostrarPedidos));
+        OnPropertyChanged(nameof(MostrarReportes));
+    }
+
+    [RelayCommand] private void VerProductos() => PaginaActual = "Productos";
+    [RelayCommand] private void VerCategorias() => PaginaActual = "Categorias";
+    [RelayCommand] private void VerProveedores() => PaginaActual = "Proveedores";
+    [RelayCommand] private void VerPedidos() => PaginaActual = "Pedidos";
+    [RelayCommand] private void VerReportes() => PaginaActual = "Reportes";
 }
